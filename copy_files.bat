4e5f2ed95a067296fa81c64a257d03f55ecc9c74
Caley Woods <caley.w@gmail.com>
2018-01-20 22:38:39 -0600

    Added script to copy game files from my code directory to the game directory where screeps watches

---
+++ copy_files.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+robocopy ./ C:\Users\Caley\AppData\Local\Screeps\scripts\127_0_0_1___21025\default
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+const spawnName = 'Spawn1';
+
 /**
-  Spawn a creep. Replace Spawn1 with your spawn name
-    spawnCreep( arrayOfParts, name )
-    Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+  Spawn our first creep if one doesn't already exist
 */
-Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
-// let myName = "Caley";
+if ( Game.spawns[spawnName].room.find(FIND_CREEPS).length === 0 ) {
+  Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+}
