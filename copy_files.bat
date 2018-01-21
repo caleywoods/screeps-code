de93b55cf555955c1c2d7994924e9c57d6594607
Caley Woods <caley.w@gmail.com>
2018-01-20 22:43:01 -0600

    make the file copy script a little better

--- copy_files.bat
+++ copy_files.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-robocopy ./ C:\Users\Caley\AppData\Local\Screeps\scripts\127_0_0_1___21025\default
+IF EXIST %userprofile%\AppData\Local\Screeps\scripts\127_0_0_1___21025\ robocopy ./ %userprofile%\AppData\Local\Screeps\scripts\127_0_0_1___21025\default
