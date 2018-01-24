78b82268d0103fd1411dd5655d2e45467f80f3fc
Caley Woods <caley.w@gmail.com>
2018-01-23 23:40:37 -0600

    Add rudimentary working spawning code.

--- modules/Spawner.ts
+++ modules/Spawner.ts
@@ -8,36 +8,65 @@ export class Spawner {
   constructor(spawnName: string) {
     this.spawnName = spawnName;
     this.panoptes  = new Panoptes();
-    // return this.spawnName;
   }
 
-  initializeRoom() {
+  initializeRoom() : void {
     // How to rinse and repeat this logic for all types of creeps without repeating?
     if ( this.panoptes.harvesterCount === 0 ) {
 
       let nextInLine = this.panoptes.harvesterCount + 1;
 
-      let creepAttempt = Game.spawns[this.spawnName].spawnCreep([WORK, CARRY, MOVE], `Harvester${nextInLine}`, {
+      // Need to spawn based on role and give it a role name to prepend before the name
+      let creepAttempt = Game.spawns[this.spawnName].spawnCreep([WORK, CARRY, MOVE], this.getName(), {
         memory: {role: 'harvester'}
       });
 
       if ( creepAttempt === OK ) {
         console.log('Creep successfully queued for spawning.');
         this.panoptes.harvesterCount++;
+
       }
 
       if ( creepAttempt === ERR_NOT_ENOUGH_ENERGY ) {
         console.log('Not enough power to initiate creep spawning.');
       }
 
+      /*
+        No real need to do anything here yet, if spawnCreep() fails with ERR_NAME_EXISTS
+        then it will just re-execute again on the next tick and likely get a new unique name.
+      */
       if ( creepAttempt === ERR_NAME_EXISTS ) {
-        // creep name already exists - try to get a new name
         console.log(`Creep with name: Harvester${nextInLine} already exists.`);
       }
 
     }
   }
 
+  /*
+    This should be "random" enough to get going, example names seen:
+
+    harmless-ugly-7244
+    arch-batch-6915
+    extraordinary-finch-4834
+    coast-cows-9047
+  */
+  getName() : string {
+    let wordsArray = [
+      'severe','bottle','agonizing','spider','disciple','fist','cotton','live',
+      'heartbroken','harmless','ignorant','cows','addict','north','small',
+      'kingdom','original','virtual','real','sideways','snake','oxen','mouse',
+      'batch','finite','baloney','coast','donut','snow','hungry','ugly','wet',
+      'face','knot','warm','arch','heist','doctor','fear','surreal','hotter',
+      'common','extraordinary','gymnast','sad','grainy','cat','conqueror',
+      'baboon','pony','horse','centaur','liger','elastic','consumer','finch'
+    ];
+
+    let wordSoup = _.sample( wordsArray, 2 ).join( '-' );
+    let randNum  = _.random( 0,10000 )
+
+    return `${wordSoup}-${randNum}`
+  }
+
   // Each tick use Panoptes watcher data to determine what type of creeps to spawn.
-  // Somehow panoptes needs to know when creeps die to adjust the creep counts 
+  // Somehow panoptes needs to know when creeps die to adjust the creep counts
 }
